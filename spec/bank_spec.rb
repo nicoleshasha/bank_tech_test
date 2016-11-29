57fc4375269c463b58d939455ab5dad689277b6b
Nicole <nicoleshasha@hotmail.co.uk>
2016-11-29 11:33:49 +0000

    Add rspec for statemnt

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,9 +2,9 @@ require 'bank'
 
 describe Bank do
 
-  it 'has no money on initialize' do
+  it 'has a bank statement menu' do
     bank = Bank.new
-    expect(bank.money).to eq []
+    expect(bank.bank_statement).to eq "date || credit || debit || balance"
   end
 
 end
